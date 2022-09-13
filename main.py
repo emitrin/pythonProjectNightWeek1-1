48d53a1450c12c9296ee8e69f8d0068efeadef52
emily <ehoacc@gmail.com>
2022-09-12 20:40:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+yourname = input("What is your name:")
+yourmajor = input("What is your major?")
+print("Hello", yourname, "comp151 will help with", yourmajor)
+comment_to_user = f"wow {yourname} comp151 will really help you for {yourmajor}"
+print(comment_to_user)
